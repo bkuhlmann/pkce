0bffb0d3e519017181b7dc4149ba216942e62302
Brooke Kuhlmann <brooke@alchemists.io>
2022-12-24 07:36:04 -0700

    Refactored code as data object instead of struct
    Necessary to improve performance further since additional struct
    features are not required.

--- lib/pkce/code.rb
+++ lib/pkce/code.rb
@@ -2,10 +2,5 @@
 
 module PKCE
   # Encapsulates the challenge and verify code.
-  Code = Struct.new :challenge, :verify, keyword_init: true do
-    def initialize *arguments
-      super
-      freeze
-    end
-  end
+  Code = Data.define :challenge, :verify
 end
